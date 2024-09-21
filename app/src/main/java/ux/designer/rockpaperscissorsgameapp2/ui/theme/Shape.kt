a87a69b9bfd9eb5d8ec6e7bf2b3b4a7c1ae22b6e
TSRWEEBDESIGN <thomas@tsr-webdesign.no>
2024-09-21 20:34:36 +0200

    Initial commit

---
+++ app/src/main/java/ux/designer/rockpaperscissorsgameapp2/ui/theme/Shape.kt
@@ -0,0 +1,11 @@
+package ux.designer.rockpaperscissorsgameapp2.ui.theme
+
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Shapes
+import androidx.compose.ui.unit.dp
+
+val Shapes = Shapes(
+    small = RoundedCornerShape(4.dp),
+    medium = RoundedCornerShape(12.dp),  // For buttons and cards
+    large = RoundedCornerShape(0.dp)
+)
